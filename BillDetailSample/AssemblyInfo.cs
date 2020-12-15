a82bd70d596085aa5f97b229598a12705b2d6dfe
davidortinau <david.ortinau@microsoft.com>
2020-12-15 12:21:14 -0600

    new icon font

--- BillDetailSample/AssemblyInfo.cs
+++ BillDetailSample/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("ProductSans-Regular.ttf", Alias = "ProductSans")]
+[assembly: ExportFont("typicons.ttf", Alias = "Typicons")]
